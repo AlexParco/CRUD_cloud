706da6961fb9f1102e7de507145ea2b90c8c771f
Alexander Parco <alexparco16@gmail.com>
2023-03-31 09:54:20 -0500

    change port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParse = require('body-parser')
 const Pool = require('pg').Pool
 const app = express()
-const PORT = 4040
+const PORT = process.env.PORT || 5000
 
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({
